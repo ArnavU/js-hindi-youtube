b461ff5851c2af3f5bff052fd23747f8a29a2c18
Arnav Umarkar <141582030+ArnavU@users.noreply.github.com>
2023-12-25 12:16:03 +0000

    random() formula marked imp

--- 01_basics/06_nums_and_math.js
+++ 01_basics/06_nums_and_math.js
@@ -28,7 +28,11 @@ console.log(Math.random());
 console.log((Math.random()*10) + 1);
 console.log(Math.floor(Math.random()*10) + 1);
 
-const min = 10
-const max = 20
+const min = 8;
+const max = 21;
 
-console.log(Math.floor(Math.random() * (max - min + 1)) + min)
+console.log(Math.floor(Math.random() * (max - min + 1)) + min);
+
+/* 
+Imp formula - console.log(Math.floor(Math.random() * (max - min + 1)) + min);
+*/
